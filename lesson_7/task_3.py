27fc19b11dace96b702077e9f4383c1784d2adbe
Ampel2511 <konstantinkail349@gmail.com>
2021-06-15 10:16:34 +0300

    lesson_7

---
+++ lesson_7/task_3.py
@@ -0,0 +1,62 @@
+"""
+3. Массив размером 2m + 1, где m – натуральное число, заполнен случайным образом.
+Найдите в массиве медиану. Медианой называется элемент ряда, делящий его на
+две равные по длине части: в одной находятся элементы, которые не меньше медианы,
+в другой – не больше медианы.
+
+Задачу можно решить без сортировки исходного массива.
+
+Но если это слишком сложно, то используйте метод сортировки,
+который не рассматривался на уроках: Шелла, Гномья, Кучей...
+
+[5, 3, 4, 2, 5, 3, 1]
+
+[3, 3, 3, 3, 3, 4, 5]
+
+my_lst
+new_lts
+
+arr[m]
+
+
+from statistics import median
+
+[3, 4, 3, 3, 5, 3, 3]
+
+left = []
+right = []
+
+left == right and
+
+for i in
+    for
+    left == right
+    left.clear()
+    right.clear()
+
+
+"""
+from random import randint
+
+
+n = int(input('Укажите длинну массива: '))
+orig_list = [randint(-100, 100) for _ in range(2*n + 1)]
+
+
+def gnome(data):
+    i, size = 1, len(data)
+    while i < size:
+        if data[i - 1] <= data[i]:
+            i += 1
+        else:
+            data[i - 1], data[i] = data[i], data[i - 1]
+            if i > 1:
+                i -= 1
+    return data
+
+
+print(orig_list)
+print(gnome(orig_list))
+print(f'Медиана: {gnome(orig_list)[n]}')  # или gnome(orig_list)[len(orig_list) // 2]
+
+
